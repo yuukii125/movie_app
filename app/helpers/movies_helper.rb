c8798d9ed99aab0586fb38265469020a1134e27e
yuukii125 <y.kipro125@gmail.com>
2021-08-16 22:53:02 +0900

    ヘルパーメソッドの作成

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,12 @@
 module MoviesHelper
+  def embed_youtube(url)
+    tag.iframe(
+      width: 560,
+      height: 315,
+      src: url,
+      frameborder: 0,
+      allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
+      allowfullscreen: true
+    )
+  end
 end
